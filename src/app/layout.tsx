3685ae4cddc78c395074d04b6359dbe0fc82c3bf
Agus Puryanto <aguspuryanto@gmail.com>
2026-09-09 02:48:23 +0700

    update: remove aichat at jurnal branch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
         {children}
-        <AIChatWidget />
+        {/* <AIChatWidget /> */}
       </body>
     </html>
   );
